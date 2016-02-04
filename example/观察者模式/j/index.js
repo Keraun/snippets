6c46c424bd8c05288ea27ad922c49765446cc751
iwuly <iwuly93@163.com>
2016-02-04 12:14:03 +0800

    观察者模式

---
+++ example/观察者模式/j/index.js
@@ -0,0 +1,34 @@
+(function($, window, undefined) {
+  //观察者模式示例
+  var ui = {
+    $btn_pub: $('#btn_pub'),
+    $btn_unsub: $('#btn_unsub')
+  }
+
+  var oPage = {
+    init: function() {
+      this.view();
+      this.listen();
+    },
+    view: function() {
+
+    },
+    listen: function() {
+
+      //点击发布事件按钮,触发事先订阅的事件
+      ui.$btn_pub.on('click', function() {
+        var argument = ['param01','param02'];
+        EventCenter.pub('/test/ls', argument);
+      });
+
+      //点击取消订阅按钮(事件取消后,点击发布事件按钮失效)
+      ui.$btn_unsub.on('click', function() {
+        EventCenter.unsub('/test/ls');
+      });
+
+    }
+  }
+
+  oPage.init();
+
+})(jQuery, window);
